0090180c090bc89acba2ff95b2a8a23958710954
je33ica <je331687@gmail.com>
2021-02-01 16:48:09 +0000

    working on base files

---
+++ controllers/burgerController.js
@@ -0,0 +1,15 @@
+var express = require("express");
+
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
