654cf605550d0a10e6470689e2836572a13aa68c
je33ica <je331687@gmail.com>
2021-02-04 14:45:03 +0000

    working on post

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -1,29 +1,58 @@
 var express = require("express");
+const burgers = require("../models/burger.js");
 
 var router = express.Router();
-var burger = require("../models/burger.js");
+
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-    burger.all(function(data) {
-      var hbsObject = {
-        burgers: data
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
-    });
+  res.redirect("/burgers")
+});
+
+router.get("/burgers", function(req, res) {
+
+
+  burgers.selectAll(burgers => {
+    res.render("index", burgers );
+  });
+    // burgers.selectAll(function(data) {
+
+    //   var hbsObject = { burgers: data };
+    
+    //   console.log("these are the burgers",hbsObject);
+    
+    //   res.render("index",hbsObject );
+    // });
   });
 
+//   // Route get route.
+// router.get('/', (req, res) => {
+//     connection.query('SELECT * FROM burgers;', (err, burgers) => {
+//       if (err) {
+//         throw err;
+//       }
+  
+//       // Test it.
+//       // console.log('The solution is: ', data);
+  
+//       // Test it.
+//       // res.send(data);
+  
+//       res.render('index', { burgers });
+//     });
+//   });
+
   router.post('/api/burgers', (req, res) => {
-    burger.create(['burger_name'], [req.body.name], (result) => {
+    burgers.create(
+      req.body.burger_name, (result) => {
       // Send back the ID of the new quote
-      res.json({ id: result.insertId });
+      res.render ({ id: result.insertId });
     });
   });
 
   router.put('/api/burgers/:id', (req, res) => {
     const condition = `id = ${req.params.id}`;
-    burger.update(
+    burgers.update(
         { devoured: true }, 
         condition,
         (result) => {
@@ -40,7 +69,7 @@ router.get("/", function(req, res) {
         const condition = `id = ${req.params.id}`;
      //   const condition = "id = " + req.params.id;
       
-        burger.delete(condition, (result) => {
+        burgers.delete(condition, (result) => {
           if (result.affectedRows === 0) {
             // If no rows were changed, then the ID must not exist, so 404
             return res.status(404).end();
@@ -50,6 +79,6 @@ router.get("/", function(req, res) {
       });
 
 
-    console.log('condition', condition);
+  //  console.log('condition', condition);
 
   module.exports = router;
