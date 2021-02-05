a420e6a334333b7632be7e7613de5665769cac23
je33ica <je331687@gmail.com>
2021-02-05 20:12:19 +0000

    fixed code for get route using id and not condition

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -1,9 +1,41 @@
-
 const express = require("express");
 const burgers = require("../models/burger.js");
 var router = express.Router();
 
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function (req, res) {
+  burgers.selectAll(function (burgers) {
+    var hbsObject = {
+      burgers: burgers,
+    };
+    // console.log("burger from controller ",hbsObject);
+    res.render("index", hbsObject);
+  });
+});
 
+// //create new burger
+// router.post('/api/burgers', (req, res) => {
+//   burgers.create(
+//     req.body.burger_name, (res) => {
+//       // Send back the ID of the new quote
+//       res.render( res );
+//     });
+// });
+
+// updating the burgers that have been devoured
+router.put("/api/burgers/:id", (req, res) => {
+  const condition = `id = ${req.params.id}`;
+  console.log("im the condition", condition);
+  burgers.update(req.params.id, function () {
+    (result) => {
+      result.status(200);
+    };
+  });
+});
+
+//  console.log('condition', condition);
+
+module.exports = router;
 
 // Create all our routes and set up logic within those routes where required.
 // router.get("/", function (req, res) {
@@ -22,46 +54,3 @@ var router = express.Router();
 //     res.render("index",hbsObject );
 //   });
 // });
-
-// Create all our routes and set up logic within those routes where required.
-router.get("/", function (req, res) {
-  burgers.selectAll(function (burgers) {
-    var hbsObject = {
-      burgers: burgers
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
-});
-
-
-// //create new burger
-// router.post('/api/burgers/create', (req, res) => {
-//   burgers.create(
-//     req.body.burger_name, (res) => {
-//       // Send back the ID of the new quote
-//       res.render( res );
-//     });
-// });
-
-// // updating the burgers that have been devoured
-// router.put('/api/burgers/:id', (req, res) => {
-//   const condition = `id = ${req.params.id}`;
-//   burgers.update(
-//     { devoured: true },
-//     condition,
-//     (result) => {
-//       if (result.changedRows === 0) {
-//         // If no rows were changed, then the ID must not exist, so 404
-//         return res.status(404).end();
-//       }
-//       res.status(200).end();
-//     }
-//   );
-// });
-
-
-
-//  console.log('condition', condition);
-
-module.exports = router;
