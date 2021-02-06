b52a6197c71da9795b7617f8e64e79a0e3148b96
je33ica <je331687@gmail.com>
2021-02-06 14:52:22 +0000

    was not calling by id but class - now fixed

--- public/js/index.js
+++ public/js/index.js
@@ -1,44 +1,54 @@
-
-
 $(document).ready(function () {
-
-
+  //for updating burgers to devoured
   $(".eatburger").on("submit", function (e) {
-    e.preventDefault()
+    e.preventDefault();
     const burgerID = $(this).children(".each").val();
 
-
     fetch(`/api/burgers/${burgerID}`, {
-      method: 'PUT',
+      method: "PUT",
       headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
-
-    }
-    ).then(() => {
-      console.log("Reload")
-
-    })
+    }).then(() => {});
     location.reload();
-  })
+  });
 
-})
+  const createBurgerFormEl = document.getElementById("newBurger");
 
-//     //updating burgers that have been eaten
-//     const eatButton = document.querySelectorAll(".eat");
-//     console.log("im the eat button ", eatButton);
+  if (createBurgerFormEl) {
+    createBurgerFormEl.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const newBurger = {
+        name: document.getElementById("burger_name").value.trim(),
+      };
 
-// const eatButtonHandler = event => {
-//   const btnID = event.target
-//   const id = btnID.getAttribute ('data-id');
-//   const availableToEat = btnID.getAttribute('data-devoured')
-//    if( availableToEat === 0){
+      console.log("I am the burger that will be created: ", newBurger);
 
+      fetch("/api/burgers", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newBurger),
+      }).then(() => {
+        document.getElementById("burger_name").value = "";
 
+        console.log("im the burgwer that was created", newBurger);
+        location.reload();
+      });
+    });
+  }
+});
 
-// }
-
+//logic for adding a burger adding a devoured set to 1 true
+// $(".newBurger").on("submit", function (e){
+//   e.preventDefault()
+//   const burgerInput = $("#burger_name")
+//   const newBurger = {
+//     burger_name: burgerInput.val().trim(),
+//     //burger_name: $('#burger_name').val().trim(),
 
 //     eatButton.forEach((button) => {
 //       button.addEventListener('click', (e) => {
@@ -71,29 +81,27 @@ $(document).ready(function () {
 //           }
 //         });
 
-
 //          // console.log("from index- id button ",id,  tobeEaten);
 
 // //console.log("im the devour button clicked", eatButton);
 //       })
 
-
 //   })
 
-  //   // CREATE
-  //   const newBurger = document.getElementById('#newBurger');
+//   // CREATE
+//   const newBurger = document.getElementById('#newBurger');
 
-  //   if (newBurger) {
-  //     eat.addEventListener('submit', (e) => {
-  //       e.preventDefault();
+//   if (newBurger) {
+//     eat.addEventListener('submit', (e) => {
+//       e.preventDefault();
 
-  //       // Grabs the value of the textarea that goes by the name, "quote"
-  //       const newBurger = {
-  //         burger_name: document.getElementById('newBurger').value.trim(),
+//       // Grabs the value of the textarea that goes by the name, "quote"
+//       const newBurger = {
+//         burger_name: document.getElementById('newBurger').value.trim(),
 
-  //       };
-  // console.log('new burger button ', newBurger);
-  //     })}
+//       };
+// console.log('new burger button ', newBurger);
+//     })}
 //     //     // Send POST request to create a new quote
 //     //     fetch('/api/burgers', {
 //     //       method: 'POST',
@@ -114,6 +122,5 @@ $(document).ready(function () {
 //     //     });
 //     //   });
 //     // }
-
 
 //   });
